bf51fac6af3b00f2fd9975f538a8e730e907c724
alexandre-balduino <alexandrebalduino17@gmail.com>
2026-04-05 18:18:08 -0300

    melhorei o exercício 001 do nível 1

--- nivel_1/ex_001_organizador_de_arquivos/main.py
+++ nivel_1/ex_001_organizador_de_arquivos/main.py
@@ -2,86 +2,58 @@
 import os
 import shutil
 
-# Define o caminho e entra na pasta
-caminho_dados = os.path.join(os.path.dirname(__file__), "dados")
-os.chdir(caminho_dados)
 
-# Cria a pasta onde os dados serão organizados
-os.makedirs("dados_organizados", exist_ok=True)
+def organizar_pasta(caminho, destino=os.path.dirname(__file__)):
+    for arq in os.listdir(caminho):
+        caminho_arquivo = os.path.join(caminho, arq)
+        nome_arquivo = os.path.basename(caminho_arquivo)
+        # Verifica se é um arquivo
+        if os.path.isfile(caminho_arquivo):
+            # Pega os nomes e extensões dos arquivos
+            nome, ext = os.path.splitext(nome_arquivo)
+            # Verifica se é uma foto
+            if ext.lower() in (".jpg", ".jpeg", ".png"):
+                # Cria a pasta imagens/ caso não exista
+                os.makedirs(os.path.join(destino, "imagens"), exist_ok=True)
+                # Move o arquivo para a pasta imagens/
+                print(f"Movendo {nome_arquivo} para imagens/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "imagens", nome_arquivo))
+            # Verifica se é um audio
+            elif ext.lower() in (".mp3"):
+                # Cria a pasta audios/ caso não exista
+                os.makedirs(os.path.join(destino, "audios"), exist_ok=True)
+                # Move o arquivo para a pasta audios/
+                print(f"Movendo {nome_arquivo} para audios/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "audios", nome_arquivo))
+            # Verifica se é um arquivo de texto
+            elif ext.lower() in (".pdf", ".txt", ".doc", ".docx"):
+                # Cria a pasta documentos/ caso não exista
+                os.makedirs(os.path.join(destino, "documentos"), exist_ok=True)
+                # Move o arquivo para a pasta imagens/
+                print(f"Movendo {nome_arquivo} para documentos/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "documentos", nome_arquivo))
+            # Verifica se é uma planilha
+            elif ext.lower() in (".xls", ".xlsx", ".csv", ".ods"):
+                # Cria a pasta planilhas/ caso não exista
+                os.makedirs(os.path.join(destino, "planilhas"), exist_ok=True)
+                # Move o arquivo para a pasta planilhas/
+                print(f"Movendo {nome_arquivo} para planilhas/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "planilhas", nome_arquivo))
+            # Verifica se é um vídeo
+            elif ext.lower() in (".mp4"):
+                # Cria a pasta videos/ caso não exista
+                os.makedirs(os.path.join(destino, "videos"), exist_ok=True)
+                # Move o arquivo para a pasta videos/
+                print(f"Movendo {nome_arquivo} para videos/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "videos", nome_arquivo))
+            # Se for um arquivo diferente
+            else:
+                # Cria a pasta outros/ caso não exista
+                os.makedirs(os.path.join(destino, "outros"), exist_ok=True)
+                # Move o arquivo para a pasta outros/
+                print(f"Movendo {nome_arquivo} para outros/")
+                shutil.move(caminho_arquivo, os.path.join(destino, "outros", nome_arquivo))
 
-pastas_criadas = [] # Inicia uma lista vazia que conterá os nomes das pastas a serem criadas
-# Percorre todos os arquivos e pastas no diretório atual
-for arq in os.listdir():
-    # Verifica se é um arquivo
-    if os.path.isfile(arq):
-        # Pega os nomes e extensões dos arquivos
-        nome, ext = os.path.splitext(arq)
-        # Se a extensão for de uma foto, adiciona "imagens" a lista pastas_criadas
-        if ext.lower() in (".jpg", ".jpeg", ".png"):
-            if "imagens" not in pastas_criadas:
-                pastas_criadas.append("imagens")
-        # Se a extensão for de um audio, adiciona "audios" a lista pastas_criadas
-        elif ext.lower() in (".mp3"):
-            if "audios" not in pastas_criadas:
-                pastas_criadas.append("audios")
-        # Se a extensão for de um arquivo de texto, adiciona "documentos" a lista pastas_criadas
-        elif ext.lower() in (".pdf", ".txt"):
-            if "documentos" not in pastas_criadas:
-                pastas_criadas.append("documentos")
-        # Se a extensão for de uma planilha, adiciona "planilhas" a lista pastas_criadas
-        elif ext.lower() in (".xls", ".xlsx", ".csv", ".ods"):
-            if "planilhas" not in pastas_criadas:
-                pastas_criadas.append("planilhas")
-        # Se a extensão for de um video, adiciona "videos" a lista pastas_criadas
-        elif ext.lower() in (".mp4"):
-            if "videos" not in pastas_criadas:
-                pastas_criadas.append("videos")
-        # Se for uma extensão diferente, adiciona "outros" a lista pastas_criadas
-        else:
-            if "outros" not in pastas_criadas:
-                pastas_criadas.append("outros")
 
-# Entra na pasta dados_organizados/
-os.chdir("dados_organizados")
-# Percorre a lista pastas_criadas
-for p in pastas_criadas:
-    # Se a pasta não existir
-    if not os.path.exists(os.path.join(caminho_dados, "dados_organizados", p)):
-        # Cria a pasta
-        os.makedirs(p, exist_ok=True)
-        print(f"pasta {p}/ criada com sucesso!")
-    else:
-        print(f"pasta {p}/ encontrada")
-# Sai da pasta dados_organizados
-os.chdir("..")
-
-# Percorre os arquivos e pastas do diretório atual
-for arq in os.listdir():
-    # Se for um arquivo
-    if os.path.isfile(arq):
-        # Pega os nomes e extensões dos arquivos
-        nome, ext = os.path.splitext(arq)
-        # Se for uma imagem
-        if ext.lower() in (".jpg", ".jpeg", ".png"):
-            # Move para a pasta imagens/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "imagens", arq))
-        # Se a extensão for de um audio
-        elif ext.lower() in (".mp3"):
-            # Move para a pasta audios/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "audios", arq))
-        # Se a extensão for de um arquivo de texto
-        elif ext.lower() in (".pdf", ".txt"):
-            # Move para a pasta documentos/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "documentos", arq))
-        # Se a extensão for de uma planilha
-        elif ext.lower() in (".xls", ".xlsx", ".csv", ".ods"):
-            # Move para a pasta planilhas/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "planilhas", arq))
-        # Se a extensão for de um video
-        elif ext.lower() in (".mp4"):
-            # Move para a pasta videos/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "videos", arq))
-        # Se for uma extensão diferente
-        else:
-            # Move para a pasta outros/
-            shutil.move(arq, os.path.join(caminho_dados, "dados_organizados", "outros", arq))
+pasta_alvo = os.path.join(os.path.dirname(__file__), "dados")
+organizar_pasta(pasta_alvo, pasta_alvo)
